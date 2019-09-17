f1d9adf57c193c6d54aa30fb58726a7a767aab40
Brian <brianflaherty64@gmail.com>
2019-09-17 11:35:14 -0400

    reducers: added schema file using normalizr

---
+++ src/reducers/schema.js
@@ -0,0 +1,25 @@
+import { schema } from 'normalizr';
+
+// export const usersSchema = new schema.Entity(
+//   'users',
+//   {},
+//   { idAttribute: '_id' }
+// );
+
+// export const messagesSchema = new schema.Entity(
+//   'messages',
+//   {},
+//   { idAttribute: '_id' }
+// );
+
+// export const notificationsSchema = new schema.Entity(
+//   'notifications',
+//   {},
+//   { idAttribute: '_id' }
+// );
+
+// export const featureTogglesSchema = new schema.Entity(
+//   'featureToggles',
+//   {},
+//   { idAttribute: '_id' }
+// );
