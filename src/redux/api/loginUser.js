072f10769ef227235aebe2f204bef08c29165d38
Brian <brianflaherty64@gmail.com>
2020-03-16 11:57:13 -0400

    feat: added loginUser actiongist

--- src/redux/api/loginUser.js
+++ src/redux/api/loginUser.js
@@ -1,8 +1,14 @@
-const loginUser = function loginUser({email, password}) {
-  return {
-    email,
-    _id: 'fakeuserid',
-  };
+import {timeout} from 'rxjs/operators';
+import request from './request';
+
+export const loginUser = function loginUser({email, password, maxTime = 5000}) {
+  const mutation = `mutation vars($email: String!, $password: String!) {
+    userLogin(email: $email, password: $password) {
+      token,
+      user { _id, birthYear, sex, forgetfulness, email }
+    }
+  }`;
+  return request(mutation, {email, password}).pipe(timeout(maxTime));
 };
 
 export default loginUser;
