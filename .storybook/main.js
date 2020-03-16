05638ac5dbc0a29af234fe9c7d5846a62c4904fd
Brian <brianflaherty64@gmail.com>
2020-03-16 11:54:36 -0400

    feat: storybook updates and improvements

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register'
+  ],
+};
